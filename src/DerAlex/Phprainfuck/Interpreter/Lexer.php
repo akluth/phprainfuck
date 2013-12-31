eb5adaf46b66ea0b747854793c789d565c71d9ab
Alexander Kluth <contact@alexanderkluth.com>
2013-12-31 04:56:23 +0100

    Forgot < and > when lexing the file
    Signed-off-by: Alexander Kluth <contact@alexanderkluth.com>

--- src/DerAlex/Phprainfuck/Interpreter/Lexer.php
+++ src/DerAlex/Phprainfuck/Interpreter/Lexer.php
@@ -10,7 +10,7 @@ class Lexer
         // Remove whitespace
         $string = preg_replace('/\s+/m', '', $string);
 
-        preg_match_all('/[+\-\[\]\.\,]/', $string, $token);
+        preg_match_all('/[+\-\[\]\.\,]<>/', $string, $token);
 
         // preg_match_all is returning an array with one element - the array
         // with the values we actual want...stupid behaviour
