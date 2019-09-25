bb6b5238c9f00f483bd6f97823ae8abd21c9be1b
Alexander Kluth <alex@clooze.io>
2019-09-25 12:43:10 +0200

    Add namespace fix and add first test
    Signed-off-by: Alexander Kluth <alex@clooze.io>

--- tests/HelloWorldInterpreterTest.php
+++ tests/HelloWorldInterpreterTest.php
@@ -1,12 +1,20 @@
 <?php
 declare(strict_types=1);
 
+use AlexClooze\Phprainfuck\Phprainfuck;
 use PHPUnit\Framework\TestCase;
 
 final class EmailTest extends TestCase
 {
     public function testCanInterpreteAndExecuteHelloWorldBrainfuckCode(): void
     {
-
+        $brainfuck = new Phprainfuck();
+        $code = <<<EOT
++++++ +++[- >++++ ++++< ]>+++ +++++ .<+++ ++[-> +++++ <]>++ ++.++ +++++
+..+++ .<+++ +++++ [->-- ----- -<]>- ----- ----- ----. <++++ +++[- >++++
++++<] >++++ ++.<+ +++[- >++++ <]>++ +++++ +.+++ .---- --.-- ----- -.<++
++++++ +[->- ----- --<]> ---.< 
+EOT;
+        $this->assertEquals("Hello World!", $brainfuck->evaluate($code));
     }
 }
